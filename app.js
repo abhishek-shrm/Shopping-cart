1e67e1b14e728a5cbf95139897c2d268f6098cc5
ABHISHEK SHARMA <abhishek.shrm.2195@gmail.com>
2019-04-03 04:11:17 +0530

    added more packages

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var mongoose=require('mongoose');
 var config=require('./config/database');
 var pages=require('./routes/pages');
 var adminPages=require('./routes/adminPages');
+var bodyParser=require('body-parser');  
+var session=require('express-session');
+var expressValidator=require('express-validator');
 var app=express();
 
 mongoose.connect(config.database,{useNewUrlParser:true});
@@ -17,6 +20,41 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+
 
 app.use('/',pages);
 app.use('/admin/pages',adminPages);
